a80e384d9928fe1147ccbd85826e9b072ecc3163
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-03-10 10:44:47 +0530

    Create CreatingObject.java

---
+++ Assignment6/CreatingObject.java
@@ -0,0 +1,13 @@
+package Assignment6;
+
+public class CreatingObject {
+
+	public static void main(String[] args) {
+		//String color, int model, String bodyType, String fuelType
+		Car tesla=new Car("White",2019,"steel","electric");
+		Car harrier=new Car("Black",2021,"steel","diesel");
+		Car Benz=new Car("C-class",2022,"steel","petrol");
+    System.out.println(tesla);
+	}
+
+}
